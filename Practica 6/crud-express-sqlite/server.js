27bc09cadce96aad81d9cd41ab86e5efe5dd15a7
Miguel Lozano <42401866@uaz.edu.mx>
2026-03-09 11:56:06 -0600

    Se elimino practica 6

--- Practica 6/crud-express-sqlite/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./src/app");
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`✅ API + UI corriendo en http://localhost:${PORT}`);
-});
--- Practica 6/crud-express-sqlite/src/middlewares/error.js
+++
@@ -1,8 +0,0 @@
-exports.notFound = (req, res) => {
-  res.status(404).json({ error: "Ruta no encontrada" });
-};
-
-exports.errorHandler = (err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ error: "Error interno del servidor" });
-};
